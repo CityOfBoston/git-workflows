356973eee61f21ec781d9b31670fbcd97df97342
albertboston <39740303+albertboston@users.noreply.github.com>
2018-12-14 10:12:52 -0500

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,11 +1,15 @@
+import numpy as np
+
 print("Starting pytest")
 
 def inc(x):
     return x + 1
 
-
 def test_answer():
     assert inc(3) == 5
-    
+
+def nump_test_answer():
+    y = np.array([3,4])
+    assert inc(y[0]) == y[1]
     
 print("Testing complete")
